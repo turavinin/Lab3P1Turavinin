47b5f26efee1ff1f0dcef3619cba06663d26bcc0
Anton <aturavinin@gmail.com>
2022-05-06 18:42:24 -0300

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+// JSON ARRAY
+var jsonBody = `[{"id":1,"dni":17663295,"nombre":"FABIAN MARCELO","apellido":"ABADIE","cursoNumero":1,"cursoLetra":"F"},
+{"id":2,"dni":38724762,"nombre":"MAIRA DAIANA","apellido":"ABALOS","cursoNumero":3,"cursoLetra":"M"},
+{"id":3,"dni":25447357,"nombre":"NOELIA LIDIA","apellido":"ABBA","cursoNumero":2,"cursoLetra":"N"},
+{"id":4,"dni":27577699,"nombre":"MARÍA SOLEDAD","apellido":"ACHOR","cursoNumero":2,"cursoLetra":"M"},
+{"id":900,"dni":11496581,"nombre":"JOSE MIGUEL","apellido":"ARMALEO","materia":"Fisica","año":1},
+{"id":899,"dni":35326658,"nombre":"ROSA DEL VALLE","apellido":"LOPEZ","materia":"Lengua","año":3},
+{"id":898,"dni":39638351,"nombre":"DANIELA BELEN","apellido":"BROGGI D'ATENA","materia":"Matematica","año":3},
+{"id":897,"dni":17275566,"nombre":"PABLO ALBERTO","apellido":"ALMEIDA","materia":"Quimica","año":1}]`;
+
